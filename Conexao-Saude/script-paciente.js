e1cd5d34a1cab1ce43a0775bac9532c085a80e43
Fernando Galvão Willemann <96529800+ferwillemann@users.noreply.github.com>
2024-10-05 15:48:01 -0300

    Delete Conexao-Saude directory

--- Conexao-Saude/script-paciente.js
+++
@@ -1,7 +0,0 @@
-const laudoBt = document.querySelectorAll('.botao-expandir')
-
-laudoBt.forEach(function (botao) {
-    botao.addEventListener('click', function () {
-      alert('deu certo!!');
-    });
-  });
